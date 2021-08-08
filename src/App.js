beb842d0cfccb0de223f6636aee4e5546527450a
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-08-08 15:27:43 -0500

    a

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+    </div>
+  );
+}
+
+export default App;
