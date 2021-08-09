87afc9738c827c2e1b38475718b2e8c611f0f6a9
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-08-08 22:08:22 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
 
 function App() {
+  function say(event) {
+    let oks = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+    let random = Math.random();
+    let second = Math.floor(random * 10);
+    ok = oks[second];
+  }
+  let ok = "";
   return (
-    <div className="App">
-      <nav class="navbar navbar-light bg-light">
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">React cul</a>
+    <a class="navbar-brand" href="/">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">thing 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">thing 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">thing 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">thing 4</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
+    <div class="compliment">
+    <button type="button" class="btn btn-primary" onClick={say}>say something</button>
+    <p>{ok}</p>
     </div>
+</div>
   );
 }
 
