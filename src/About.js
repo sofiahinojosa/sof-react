b99f6b9c433ef3582b1711ffee7b4501798698aa
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-08-26 13:02:03 -0500

    a

--- src/About.js
+++ src/About.js
@@ -7,6 +7,39 @@ function About() {
     <div className="About">
       <h3>About me ✌️</h3>
       <p>I am a 14 year old web developer from Mexico. My coding journey started in quarantine about a year ago with html, css, and js. Currently, I am learning Angular. I like working with React too :D On my free time I like to sail, listen to music, make diys, read and code. I have a berry good end awe some sister end ay have some berry wonder full dogs nei md mee k i nee na ai donttttt wannaaaaaa and i dontttt wanttttt to wanttt uuuuuuuuu -bili zeja. mice sister izzi berry cul be cause and effect chi guatatachehactachetechametahctes mira ciulouiuils uwu ponle nooo no le pongas ponleee nooooooo :c no le pongas nooo no ponle noooo aaaa deja de ponernolepongastodoeso *squeak* bueno ahora si ponle no le pongas ponle aa a a a *c va volando* mice sister is berry not super cul</p>
+
+      <h3>My skills ⚡️</h3>
+      <div className="skills">
+      <ul>
+        <li className="blue">
+          React
+        </li>
+        <li className="pink">
+          HTML
+        </li>
+        <li className="yellow">
+          Typescript
+        </li>
+        <li className="green">
+          Github
+        </li>
+        <li className="pink">
+          Css
+        </li>
+        <li className="yellow">
+          JavaScript
+        </li>
+        <li className="blue">
+          Angular
+        </li>
+        <li className="green">
+          Bootstrap
+        </li>
+        <li className="blue">
+          Git
+        </li>
+      </ul>
+      </div>
       <Footer />
     </div>
   );
