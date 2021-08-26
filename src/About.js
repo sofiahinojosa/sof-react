11694960a623b3b3e5327a8844e398e38b2353da
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-08-26 16:16:23 -0500

    fixed "my skills" section for mobile

--- src/About.js
+++ src/About.js
@@ -9,36 +9,40 @@ function About() {
       <p>I am a 14 year old web developer from Mexico. My coding journey started in quarantine about a year ago with html, css, and js. Currently, I am learning Angular. I like working with React too :D On my free time I like to sail, listen to music, make diys, read and code. I have a berry good end awe some sister end ay have some berry wonder full dogs nei md mee k i nee na ai donttttt wannaaaaaa and i dontttt wanttttt to wanttt uuuuuuuuu -bili zeja. mice sister izzi berry cul be cause and effect chi guatatachehactachetechametahctes mira ciulouiuils uwu ponle nooo no le pongas ponleee nooooooo :c no le pongas nooo no ponle noooo aaaa deja de ponernolepongastodoeso *squeak* bueno ahora si ponle no le pongas ponle aa a a a *c va volando* mice sister is berry not super cul</p>
 
       <h3>My skills ⚡️</h3>
+      <div class="container">
       <div className="skills">
+        <div class="row">
       <ul>
-        <li className="blue">
+        <p className="blue col">
           React
-        </li>
-        <li className="pink">
+        </p>
+        <p className="yellow col">
           HTML
-        </li>
-        <li className="yellow">
+        </p>
+        <p className="pink col">
           Typescript
-        </li>
-        <li className="green">
+        </p>
+        <p className="green col">
           Github
-        </li>
-        <li className="pink">
+        </p>
+        <p className="pink col">
           Css
-        </li>
-        <li className="yellow">
+        </p>
+        <p className="yellow col">
           JavaScript
-        </li>
-        <li className="blue">
+        </p>
+        <p className="blue col">
           Angular
-        </li>
-        <li className="green">
+        </p>
+        <p className="green col">
           Bootstrap
-        </li>
-        <li className="blue">
+        </p>
+        <p className="blue col">
           Git
-        </li>
+        </p>
       </ul>
+      </div>
+      </div>
       </div>
       <Footer />
     </div>
