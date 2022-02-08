0e18b533bb5480f2bec104f3e49d1746fce921b3
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-02-07 21:35:38 -0600

    f

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
   return(
-    <div>
-      <h1>hai</h1>
+    <div className="App">
+      <h1>hey there :)</h1>
     </div>
   )
 }
